df48e751076239cd1cc993574a040f0e59680088
Clayton <ctvanoostwaard@gmail.com>
2025-03-31 19:11:38 -0400

    Made a few changes to the app. Installed and added environ

--- home_inventory/settings.py
+++ home_inventory/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)a6h!9@brm9l%x_e^gx*l3^c)$t*3hw_dqj&$!(2$pe#+$g06'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = []
 
